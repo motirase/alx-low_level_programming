3ca0330c1d04e0acb4217175efa372ec7a3486c6
motirase <motirase2022@gmail.com>
2022-12-08 01:03:35 -0800

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,21 +1,13 @@
 #include <stdio.h>
-
 /**
  *
- *  *main - entry point
+ *main - entry point
  *
- *   *
- *
- *    *Return: 0
- *
- *     */
-
+ *Return: 0
+ */
 int main(void)
-
 {
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
+	return (0);
 }
