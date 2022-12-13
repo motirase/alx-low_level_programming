1916d4b072bd0ec28e7b4a40cb629c4e848980b3
motirase <motirase2022@gmail.com>
2022-12-12 23:55:57 -0800

    ten times apha

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){ 
+       	print_alphabet_x10(void);                                            	                 }
+       void print_alphabet_x10(void){
+       char letter;
+       for(int i=0;i<10;i++){
+       for(letter = 'a'; letter <='z'; letter++){
+       _putchar(letter);
+       }       
+       }
+       }
