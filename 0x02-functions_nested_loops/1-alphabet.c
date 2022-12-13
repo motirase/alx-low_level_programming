08cf020c14dd1a0f2d720132ccc31608c95130c2
motirase <motirase2022@gmail.com>
2022-12-13 04:45:17 -0800

    oopp

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,15 @@
-#include <stdio.h>
-int main (){
+#include "main.h"
+/**
+ *print_alphabet - print alphabet lowercase
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
 	char letter;
 	for(letter ='a'; letter <='z'; letter++){
 		_putchar(letter);
-		putchar('\n');
+	
 	}
 
+	_putchar('\n');
