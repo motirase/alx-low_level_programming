2d5539e33675826aed9e9d2bd4c45a8d65a8352e
motirase <117908555+motirase@users.noreply.github.com>
2022-12-13 15:13:02 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,14 @@
-nclude "holberton.h"
+#include "main.h"
 int main (){
-	char ch = 'C';
-	putchar(ch);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);	
+}
