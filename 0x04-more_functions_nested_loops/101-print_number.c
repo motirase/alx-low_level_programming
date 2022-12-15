191327c59aa185690872e87c3394552d85aeb7b1
motirase <117908555+motirase@users.noreply.github.com>
2022-12-15 15:25:12 +0300

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,57 +1,21 @@
-#include "lists.h"
-#include <stdlib.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
- * find_listint_loop_pl - finds a loop in a linked list
- *
- * @head: linked list to search
- *
- * Return: address of node where loop starts/returns, NULL if no loop
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
  */
-listint_t *find_listint_loop_pl(listint_t *head)
+void print_number(int n)
 {
-	listint_t *ptr, *end;
+	unsigned int num = n;
 
-	if (head == NULL)
-		return (NULL);
-
-	for (end = head->next; end != NULL; end = end->next)
+	if (n < 0)
 	{
-		if (end == end->next)
-			return (end);
-		for (ptr = head; ptr != end; ptr = ptr->next)
-			if (ptr == end->next)
-				return (end->next);
-	}
-	return (NULL);
-}
-
-/**
- * print_listint_safe - prints a linked list, even if it
- * has a loop
- *
- * @head: head of list to print
- *
- * Return: number of nodes printed
- */
-size_t print_listint_safe(const listint_t *head)
-{
-	size_t len = 0;
-	int loop;
-	listint_t *loopnode;
-
-	loopnode = find_listint_loop_pl((listint_t *) head);
-
-	for (len = 0, loop = 1; (head != loopnode || loop) && head != NULL; len++)
-	{
-		printf("[%p] %d\n", (void *) head, head->n);
-		if (head == loopnode)
-			loop = 0;
-		head = head->next;
+		_putchar('-');
+		num = -num;
 	}
 
-	if (loopnode != NULL)
-		printf("-> [%p] %d\n", (void *) head, head->n);
-	return (len);
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
 }
