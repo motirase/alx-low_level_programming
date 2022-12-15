408d2d852ce68303d5fd3ce2709543b1c31fa986
motirase <117908555+motirase@users.noreply.github.com>
2022-12-15 15:38:14 +0300

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,13 +4,13 @@
  * print_number - Prints an integer.
  * @n: The integer to be printed.
  */
-void print_number(void)
+void print_number(int n)
 {
 	int i ;
 	
 	i = 0;
 	
-	for(i = 0; i<10; i++)
+	for(i = 0; i<n; i++)
 		_putchar(i);
 	 	_putchar("\n");
 }
