0ef3f64c97756eb40c5f6105eb7df4a2fca7cc3c
motirase <117908555+motirase@users.noreply.github.com>
2022-12-15 15:15:30 +0300

    ttooo

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,28 +1,28 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
- * print_number - print an integer (with no trailing newline)
- * @n: integer to print
- *
- * Return: void
- */
+  * print_number - Prints an integer.
+  * @n: The integer to prints.
+  *
+  * Return: Nothing!
+  */
 void print_number(int n)
 {
-	unsigned int m;
+	unsigned int k = n;
 
-	if (n >= 0)
+	if (n < 0)
 	{
-		if (n >= 10)
-			print_number(n / 10);
-		_putchar('0' + n % 10);
-	}
-	else
-	{
+		n *= -1;
+		k = n;
 		_putchar('-');
-		m = -1 * n;
-		if (m >= 10)
-			print_number(m / 10);
-		_putchar('0' + m % 10);
 	}
+
+	k /= 10;
+
+	if (k != 0)
+		print_number(k);
+
+	_putchar((unsigned int) n % 10 + '0');
 
 }
