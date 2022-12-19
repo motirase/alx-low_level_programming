7a8d02bfbdf72158440c26e1b5a10243f0d8ad89
motirase <117908555+motirase@users.noreply.github.com>
2022-12-19 13:34:33 +0300

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,19 @@
-/*
- *File: 3-puts.c
- *Auth: Brennan D Baraban
- */
-
 #include "main.h"
-
 /**
- *_puts - Prints a string to stdout.
- *@str: The string to be printed.
+ * _puts - print a string
+ * @str: a pointer to int
+ * Return: void method
  */
 void _puts(char *str)
 {
-	while (*str)
-	_putchar(*str++);
-	
+	char *c;
+	int m;
+
+	c = str;
+
+	for (m = 0; c[m]; m++)
+	{
+		_putchar (c[m]);
+	}
 	_putchar('\n');
 }
