175db12d4400ebd8831f441ac686db256cddf210
motirase <117908555+motirase@users.noreply.github.com>
2023-01-19 10:47:01 +0300

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,25 +2,37 @@
 #include <stdlib.h>
 
 /**
-* main - prints the opcodes of its own main function
-* @argc: argument count
-* @argv: argument vector
-*
-* Return: The job has been finished
-*/
-
-int main(int argc, char *argv[])
+ * main - function with two arguments
+ * @argc: int type argument count
+ * @argv: char type argument array
+ *
+ * Description: print opcode
+ * Return: na
+ */
+int main(int argc, char **argv)
 {
+	int count;
+
+	count = 0;
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-
 	if (atoi(argv[1]) < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
+	while (count < atoi(argv[1]))
+	{
+		printf("%02x", *((unsigned char *)main + count));
+		count++;
+		if (atoi(argv[1]) > count)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
 	return (0);
 }
